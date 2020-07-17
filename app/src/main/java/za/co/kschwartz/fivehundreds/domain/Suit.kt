bc66f6d73bf3adb684e3a7eb54dd9fdc26d939d9
Kobus Schwartz <koosschwartz7@gmail.com>
2020-07-17 22:44:06 +0200

    Introduced the concept of trumpWeight to indicate suit hierarchy as trumps.

--- app/src/main/java/za/co/kschwartz/fivehundreds/domain/Suit.kt
+++ app/src/main/java/za/co/kschwartz/fivehundreds/domain/Suit.kt
@@ -1,5 +1,7 @@
 package za.co.kschwartz.fivehundreds.domain
 
-enum class Suit(val title: String) {
-    SPADE("Spades"), CLUB("Clubs"), DIAMOND("Diamonds"), HEART("Hearts"), JOKER("Jokers"), NULLSUIT("NoSuit")
+enum class Suit(val title: String, val trumpWeight: Int) {
+    SPADE("Spades", 1), CLUB("Clubs", 2),
+    DIAMOND("Diamonds", 3), HEART("Hearts", 4 ),
+    JOKER("Jokers", 5), NULLSUIT("NoSuit", 0)
 }
