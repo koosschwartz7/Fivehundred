9c4cecac12ccab16acbbf1514d21e3a7f4c0bbe3
Kobus Schwartz <koosschwartz7@gmail.com>
2021-03-10 19:44:44 +0200

    Typo in joker-suit title.

--- app/src/main/java/za/co/kschwartz/fivehundreds/domain/Suit.kt
+++ app/src/main/java/za/co/kschwartz/fivehundreds/domain/Suit.kt
@@ -3,5 +3,5 @@ package za.co.kschwartz.fivehundreds.domain
 enum class Suit(val title: String, val trumpWeight: Int) {
     SPADE("Spades", 40), CLUB("Clubs", 60),
     DIAMOND("Diamonds", 80), HEART("Hearts", 100),
-    JOKER("Jokers", 120), NULLSUIT("NoSuit", 0)
+    JOKER("Joker", 120), NULLSUIT("NoSuit", 0)
 }
